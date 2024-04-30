0d1b2f7051c342e433e62b758a49ed4a9d7688ef
analebyodkina <dp030992cad@gmail.com>
2024-04-30 13:09:55 +0300

    making footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,38 @@
-
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js";
 // import Swiper bundle with all modules installed
-import Swiper from 'swiper/bundle';
+import Swiper from "swiper/bundle";
 
 // import styles bundle
-import 'swiper/css/bundle';
+import "swiper/css/bundle";
 
- 
-    var swiper = new Swiper(".swiper", {
-      effect: "coverflow",
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-      coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 2,
-        slideShadows: true
-      },
-      keyboard: {
-        enabled: true
-      },
-      mousewheel: {
-        thresholdDelta: 70
-      },
-      spaceBetween: 60,
-      loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-      }
-    });
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  keyboard: {
+    enabled: true,
+  },
+  mousewheel: {
+    thresholdDelta: 70,
+  },
+  spaceBetween: 60,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
-    mobileNav();
-  
+mobileNav();
+
+// copyright
+var currentYear = new Date().getFullYear();
+document.getElementById("footer-year").innerHTML = currentYear;
