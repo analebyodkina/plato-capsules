a32ad30b3086a7a211cef61227c0f34459eede4a
analebyodkina <dp030992cad@gmail.com>
2024-05-02 17:39:44 +0300

    working version before optimization with critical

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -3,11 +3,19 @@ function mobileNav() {
 	const navBtn = document.querySelector('.mobile-nav-btn');
 	const nav = document.querySelector('.mobile-nav');
 	const menuIcon = document.querySelector('.nav-icon');
+	const headerLink = document.querySelector('.header__li');
 
 	navBtn.onclick = function () {
 		nav.classList.toggle('mobile-nav--open');
 		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
+		document.body.classList.toggle('no-scroll');		
+	};
+
+	headerLink.onclick = function () {
+		nav.classList.remove('mobile-nav--open');	
+		menuIcon.classList.remove('nav-icon--active');	
+		document.body.classList.remove('no-scroll');
+		
 	};
 }
 
